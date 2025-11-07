b164e0cf769e2842c3b16c4b5d4272f5d526cee2
PhyoeKyawThan <kyawthan.phyoe9@gmail.com>
2025-11-07 17:48:14 +0630

    change: remove tracker to avoid not found class attribute images

--- helpers/sync.py
+++ helpers/sync.py
@@ -1,16 +1,41 @@
-from .tracker import Tracker
+# from .tracker import Tracker
 from models import db, Employee, Change
 from sqlalchemy import select
 from datetime import datetime
 import os
 from werkzeug.datastructures import FileStorage
-class Sync(Tracker):
+
+class Sync:
+    _device: str
+    _changed_data: dict
+    images: list[FileStorage]
+    UPLOAD_DIR: str
+    separated_data: dict = {
+        "create" : [],
+        "update" : [],
+        "delete" : [],
+    }
+    
     def __init__(self, changed_data: dict, images: list[FileStorage], upload_dir: str) -> None:
-        super().__init__(changed_data, images, upload_dir)
+        if len(changed_data['changes']) <= 0:
+            self._changed_data = None
+        else:
+            self._device = changed_data['device']
+            self._changed_data = changed_data['changes']
+            self._separate()
+            self.images = images
+            self.UPLOAD_DIR = upload_dir
+    
+    def _separate(self) -> None:
+        for d in self._changed_data:
+            if d['type'] in self.separated_data.keys():
+                self.separated_data[d['type']].append(d['data'])
     
     def apply_changes(self) -> bool:
         for type in self.separated_data.keys():
             self._changes_mapper(type, self.separated_data[type])
+        db.session.query(Change).delete()
+        db.session.commit()
     
     def _changes_mapper(self, type: str, data: list):
         if type == "create":
